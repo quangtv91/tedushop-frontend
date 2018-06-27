af5af906387bb09cb97ca5a341bb4d701e72221c
unknown <quangtv91>
2018-06-27 13:38:18 +0700

    done leson11

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { UserModule } from './user/user.module';
+import { RouterModule } from '@angular/router';
+import { mainRoutes } from './main.routes';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    UserModule,
+    RouterModule.forChild(mainRoutes),
   ],
   declarations: [MainComponent]
 })
