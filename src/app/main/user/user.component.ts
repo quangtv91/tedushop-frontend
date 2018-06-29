22f71d110792861af762f88919e6301ae800fc96
unknown <quangtv91>
2018-06-29 23:29:38 +0700

    done lesson27

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -5,6 +5,8 @@ import { NotificationService } from '../../core/services/notification.service';
 import { MessageContstants } from '../../core/common/message.constants';
 import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
 
+declare var moment: any;
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -68,6 +70,11 @@ export class UserComponent implements OnInit {
     this._dataService.get('/api/appUser/detail/' + id)
       .subscribe((res: any) => {
         this.entity = res;
+        // gan role hien tai cua user
+        for (let role of this.entity.Roles) {
+          this.myRoles.push(role);
+        }
+        this.entity.BirthDay = moment(new Date(this.entity.BirthDay)).format('DD/MM/YYYY');
       });
   }
 
